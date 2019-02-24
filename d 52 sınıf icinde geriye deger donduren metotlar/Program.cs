7ca37f7bd9fd9a425232fd654d585c373c7f488d
alican <doc.alican@hotmail.com>
2019-02-24 21:51:45 +0300

    sinif icinde geriye deger dondurmeyen 4 islem

---
+++ d 52 sınıf icinde geriye deger donduren metotlar/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d_52_sınıf_icinde_geriye_deger_donduren_metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            islemler isl = new islemler();
+            int s1, s2;
+       
+            {
+                // toplama
+                Console.Write("sayi 1:");
+                s1 = Convert.ToInt16(Console.ReadLine());
+                Console.Write("sayi 2:");
+                s2 = Convert.ToInt16(Console.ReadLine());
+                isl.topla(s1, s2);
+
+                // fark
+                Console.Write("sayi 1:");
+                s1 = Convert.ToInt16(Console.ReadLine());
+                Console.Write("sayi 2");
+                s2 = Convert.ToInt16(Console.ReadLine());
+                isl.cikarma(s1, s2);
+
+                //carpma
+                Console.Write("Sayi 1:");
+                s1 = Convert.ToInt16(Console.ReadLine());
+                Console.Write("sayi 2:");
+                s2 = Convert.ToInt32(Console.ReadLine());
+                isl.carpma(s1, s2);
+
+
+                //Bolme
+                Console.Write("Sayi 1:");
+                s1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Sayi 2:");
+                s2 = Convert.ToInt32(Console.ReadLine());
+                isl.bolme(s1, s2);
+
+
+
+            }
+        }
+
+
+    }
+}
---
+++ d 52 sınıf icinde geriye deger donduren metotlar/islemler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d_52_sınıf_icinde_geriye_deger_donduren_metotlar
+{
+    class islemler
+    {
+
+     public int topla(int s1,int s2)
+        {
+            int s3 = s1 + s2;
+            Console.WriteLine("Toplamı" + s3);
+            return s3;
+
+        }
+      public int cikarma(int s1,int s2)
+        {
+            int s3 = s1 - s2;
+            Console.WriteLine("Cikarma" + s3);
+            return s3;
+        }
+            public int carpma(int s1,int s2)
+        {
+            int s3 = s1 * s2;
+            Console.WriteLine("Carpma" + s3);
+            return s3;
+        }
+        public int bolme (int s1,int s2)
+        {
+            int s3 = s1 / s2;
+            Console.WriteLine("Bolme" + s3);
+            return s3;
+        }
+    }
+}
